bdda2ae36cd27149981fa9473bf823ba316cb9c4
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2024-12-29 19:22:32 +0700

    Membuat PenyediaViewModel

---
+++ app/src/main/java/com/example/p8mysqldatainternet/ui/ViewModel/PenyediaViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.p8mysqldatainternet.ui.ViewModel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.p8mysqldatainternet.MahasiswaApplications
+
+object PenyediaViewModel{
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(MahasiswaApplications().container.mahasiswaRepository) }
+        initializer { InsertViewModel(MahasiswaApplications().container.mahasiswaRepository) }
+    }
+    fun CreationExtras.MahasiswaApplications(): MahasiswaApplications=
+        (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as MahasiswaApplications)
+}
