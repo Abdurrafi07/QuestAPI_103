57ccdec02767144f1044296204b09452698d42bd
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2024-12-29 12:31:45 +0700

    Membuat MahasiswaContainer

---
+++ app/src/main/java/com/example/p8mysqldatainternet/repository/MahasiswaContainer.kt
@@ -0,0 +1,23 @@
+package com.example.p8mysqldatainternet.repository
+
+import com.example.p8mysqldatainternet.service.MahasiswaService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+interface AppContainer {
+    val mahasiswaRepository: MahasiswaRepository
+}
+
+class MahasiswaContainer : AppContainer{
+    private val baseUrl = "http://localhost/UmyTI/"
+    private val json = Json{ignoreUnknownKeys = true}
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl).build()
+
+    private val mahasiswaService: MahasiswaService by lazy { retrofit.create(MahasiswaService::class.java) }
+    override val mahasiswaRepository: MahasiswaRepository by lazy { NetworkMahasiswaRepository(mahasiswaService)}
+
+}
