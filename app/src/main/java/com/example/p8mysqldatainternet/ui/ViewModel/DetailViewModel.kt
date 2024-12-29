e066873b015bc46f7e87dd58c6f9acb9a13861d0
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2024-12-29 20:32:35 +0700

    Membuat DetailViewModel

---
+++ app/src/main/java/com/example/p8mysqldatainternet/ui/ViewModel/DetailViewModel.kt
@@ -0,0 +1,55 @@
+package com.example.p8mysqldatainternet.ui.ViewModel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
+import com.example.p8mysqldatainternet.model.Mahasiswa
+import com.example.p8mysqldatainternet.repository.MahasiswaRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed class DetailUiState {
+    object Loading : DetailUiState()
+    data class Success(val mahasiswa: Mahasiswa) : DetailUiState()
+    object Deleted : DetailUiState() // New state for delete success
+    object Error : DetailUiState()
+}
+
+class DetailViewModel(private val repository: MahasiswaRepository) : ViewModel() {
+    private val _uiState = MutableStateFlow<DetailUiState>(DetailUiState.Loading)
+    val uiState: StateFlow<DetailUiState> = _uiState
+
+    fun getMahasiswaById(nim: String) {
+        viewModelScope.launch {
+            _uiState.value = DetailUiState.Loading
+            try {
+                val mahasiswa = repository.getMahasiswaById(nim)
+                _uiState.value = DetailUiState.Success(mahasiswa)
+            } catch (e: IOException) {
+                e.printStackTrace()
+                _uiState.value = DetailUiState.Error
+            } catch (e: HttpException) {
+                e.printStackTrace()
+                _uiState.value = DetailUiState.Error
+            }
+        }
+    }
+
+    fun deleteMahasiswa(nim: String) {
+        viewModelScope.launch {
+            _uiState.value = DetailUiState.Loading
+            try {
+                repository.deleteMahasiswa(nim) // Call delete method in the repository
+                _uiState.value = DetailUiState.Deleted
+            } catch (e: IOException) {
+                e.printStackTrace()
+                _uiState.value = DetailUiState.Error
+            } catch (e: HttpException) {
+                e.printStackTrace()
+                _uiState.value = DetailUiState.Error
+            }
+        }
+    }
+}
